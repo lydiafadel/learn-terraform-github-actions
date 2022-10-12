e5bf5a7cfa6b798bb5d69067d8bd1897552096f4
lydia <105000328+lydiafadel@users.noreply.github.com>
2022-10-12 16:09:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.26.0"
-    }
-  }
-  required_version = ">= 1.1.0"
-
   cloud {
     organization = "Demo-Lydia"
-
     workspaces {
       name = "learn-terraform-github-actions"
     }
   }
-}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+  } 
+
 
 provider "aws" {
-  region = "eu-west-3"
+  region  = "eu-west-3"
 }
 
-resource "aws_instance" "web"{
+
+resource "aws_instance" "app_server"{
   ami                    = "ami-02d0b1ffa5f16402d"
   instance_type          = "t2.micro"
   subnet_id              = "subnet-0d77553dc9abeae0e"
