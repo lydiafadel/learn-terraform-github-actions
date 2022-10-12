9df3625a37d8e29037dfa85011f1f97b921b570d
lydia <105000328+lydiafadel@users.noreply.github.com>
2022-10-12 19:19:42 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,7 +46,6 @@ resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-  subnet_id              = "subnet-0d77553dc9abeae0e"
 
 }
 
