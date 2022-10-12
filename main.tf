62202335c331fd589ea290e347954b82a73e03cc
lydia <105000328+lydiafadel@users.noreply.github.com>
2022-10-12 17:10:23 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,14 +47,6 @@ resource "aws_instance" "web" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
 }
 
 resource "aws_security_group" "web-sg" {
